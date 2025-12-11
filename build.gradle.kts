3601e61bab28dd57b886c1a903c22b342f4b504b
Luca Ambrosi <luca.ambrosi@qooa.it>
2025-12-11 11:36:28 +0100

    Day 10

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,3 +13,8 @@ tasks {
         gradleVersion = "9.2.1"
     }
 }
+
+dependencies {
+    implementation("tools.aqua:z3-turnkey:4.14.1")
+    implementation("org.apache.commons:commons-math3:3.6.1")
+}
--- src/Day10.kt
+++ src/Day10.kt
@@ -1,3 +1,5 @@
+import com.microsoft.z3.Context
+import com.microsoft.z3.Status
 import utils.println
 import utils.readInput
 import kotlin.math.pow
@@ -21,7 +23,7 @@ fun main() {
     }
 
     fun part1(input: List<String>): Int {
-        return input.map { it ->
+        return input.sumOf { it ->
             val tokens = it.split(" ")
             val target = tokens.first().substringAfter("[").substringBefore("]").replace(".", "0").replace("#", "1").reversed().toInt(2)
             val buttons = tokens.drop(1).dropLast(1).map { t -> t.substringAfter("(").substringBefore(")").split(",") }
@@ -29,18 +31,35 @@ fun main() {
             combinations(buttons).filterNot { it.isEmpty() }.filter { c ->
                 c.flatten().map { s -> 2.toDouble().pow(s.toInt()).toInt() }.reduce { acc, n -> acc.xor(n) } == target
             }.minOf { it.size }
-        }.sum()
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.map { it ->
-            val tokens = it.split(" ")
-            val target = tokens.first().substringAfter("[").substringBefore("]").replace(".", "0").replace("#", "1").reversed().toInt(2)
-            val buttons = tokens.drop(1).dropLast(1).map { t -> t.substringAfter("(").substringBefore(")").split(",") }
-            val jolt = tokens.last().substringAfter("{").substringBefore("}").split(",")
+        fun String.dropBrackets() = this.replace(Regex("[\\[\\](){}]"), "")
 
-            0
-        }.sum()
+        return input.map { it.split(" ").filterNot { it.isEmpty() }.drop(1) }.sumOf { line ->
+            val target = line.last().dropBrackets().split(',').map(String::toInt)
+            val buttons = line.dropLast(1).map {
+                it.dropBrackets().split(',').map(String::toInt)
+            }
+            Context().use { ctx ->
+                val opt = ctx.mkOptimize()
+                val vars = buttons.indices.map { ctx.mkIntConst("b$it") }
+                vars.forEach { opt.Add(ctx.mkGe(it, ctx.mkInt(0))) }
+                target.indices.forEach { i ->
+                    val terms = buttons.withIndex().filter { i in it.value }.map { vars[it.index] }
+                    if (terms.isNotEmpty()) {
+                        val sum = if (terms.size == 1) terms[0]
+                        else ctx.mkAdd(*terms.toTypedArray())
+                        opt.Add(ctx.mkEq(sum, ctx.mkInt(target[i])))
+                    } else if (target[i] != 0) error("error")
+                }
+                opt.MkMinimize(ctx.mkAdd(*vars.toTypedArray()))
+                if (opt.Check() == Status.SATISFIABLE) {
+                    vars.sumOf { opt.model.evaluate(it, false).toString().toInt() }
+                } else 0
+            }
+        }
     }
 
     val input = readInput("Day10")
